bd625afbba5313732bf64979c7357447e33e5479
Dan Brellis <danbrellis@gmail.com>
2017-07-14 11:51:32 -0400

    Replaces typekit for google fonts

--- wp-content/themes/marylandnature/header.php
+++ wp-content/themes/marylandnature/header.php
@@ -31,19 +31,6 @@
 
 		<!-- Drop Google Analytics here -->
 		<!-- end analytics -->
-		
-		<!-- Le Fonts -->
-		<script>
-			/*(function(d) {
-				var config = {
-					kitId: 'euj2far',
-					scriptTimeout: 3000,
-					async: true
-				},
-				h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-			})(document);*/
-		</script>
-
 	</head>
 	
 	<!-- Uncomment this line if using the Off-Canvas Menu --> 
