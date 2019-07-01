3a8a7579de8f91f5005601927b1881b5660d1851
Dan Brellis <danbrellis@gmail.com>
2019-07-01 16:36:31 -0400

    Removes custom metabox fields/data in lieu of using ACF plugin

--- wp-content/plugins/nhsm-wa-events/nhsm-wa-events.php
+++ wp-content/plugins/nhsm-wa-events/nhsm-wa-events.php
@@ -26,22 +26,6 @@ function nhsm_wa_events_admin_init() {
     // Classes
     require_once $plugin_path . 'includes/class-wa-api-client.php';
     require_once $plugin_path . 'admin/class-events-admin.php';
-    include_once $plugin_path . 'includes/class-metaboxes.php';
     new Admin\Events_Admin();
 }
 add_action( 'admin_init', 'NHSM\\Events\\nhsm_wa_events_admin_init' );
-
-/**
- * Enqueue admin scripts and styles.
- * @param string $pagenow
- */
-function admin_scripts_styles( $pagenow ) {
-    global $post_type;
-
-    $plugin_url = plugins_url( '', __FILE__ );
-
-    if ( in_array( $pagenow, array( 'post-new.php', 'post.php' ), true ) && in_array( $post_type, apply_filters( 'em_event_post_type', array( 'event' ) ) ) ) {
-        wp_enqueue_style( 'nhsm-wa-events-admin', $plugin_url . '/css/admin.css' );
-    }
-}
-add_action( 'admin_enqueue_scripts', 'NHSM\\Events\\admin_scripts_styles' );
