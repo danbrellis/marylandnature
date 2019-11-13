60c74e7e7373a065706b6c36d425a04d18fc8009
danbrellis <danbrellis@gmail.com>
2019-11-13 15:06:27 -0500

    HTMl changes for footer to make style changes done in 1cb60e9

--- wp-content/themes/marylandnature/footer.php
+++ wp-content/themes/marylandnature/footer.php
@@ -2,33 +2,33 @@
 				<footer class="footer" role="contentinfo">
 					<div id="inner-footer" class="row">
 						<div class="medium-12 columns">
-							<div class="media-object">
-								<div class="media-object-section">
-									<a href="<?php echo site_url(); ?>" title="Home"><?php echo wp_get_attachment_image( get_field('footer_logo', 'option'), 'thumbnail', "", array( "class" => "img-responsive hide-for-small-only" ) ); ?></a>
+							<div class="footer__logoAddress">
+								<div class="logoAddress__logo">
+									<a href="<?php echo site_url(); ?>" title="Home"><?php echo wp_get_attachment_image( get_field('footer_logo', 'option'), 'thumbnail', "", array( "class" => "img-responsive" ) ); ?></a>
 								</div>
-								<div class="media-object-section">
+								<div class="logoAddress__address">
 									<span class="h4"><?php bloginfo('name'); ?></span><br />
 									<?php echo nl2br(get_field('org_info_block', 'option')); ?>
 								</div>
-								<div class="media-object-section">
-									<?php if( have_rows('social_icons', 'option') ): ?>
-									<ul class="home-social">
-										<?php while ( have_rows('social_icons', 'option') ) : the_row();
-										$fi = get_sub_field('social_media_icon');
-										$img_id = get_sub_field('social_website_custom_image');
-										$icon = '';
-										if($fi) $icon = '<i class="fi-'.$fi.'"></i>';
-										elseif($img_id) {
-											$icon = wp_get_attachment_image( $img_id, false, "" );
-										}
-										else $icon = '<i class="fi-web"></i>'
-										?>
-										<li><a href="<?php the_sub_field('social_website_url'); ?>" title="<?php the_sub_field('social_website_name'); ?>" target="_blank"><?php echo $icon; ?></a></li>
-										<?php endwhile; ?>
-									</ul>
-									<?php endif; ?>
-								</div>
 							</div>
+                            <div class="socialIconList">
+                                <?php if( have_rows('social_icons', 'option') ): ?>
+                                    <ul class="home-social">
+                                        <?php while ( have_rows('social_icons', 'option') ) : the_row();
+                                            $fi = get_sub_field('social_media_icon');
+                                            $img_id = get_sub_field('social_website_custom_image');
+                                            $icon = '';
+                                            if($fi) $icon = '<i class="fi-'.$fi.'"></i>';
+                                            elseif($img_id) {
+                                                $icon = wp_get_attachment_image( $img_id, false, "" );
+                                            }
+                                            else $icon = '<i class="fi-web"></i>'
+                                            ?>
+                                            <li><a href="<?php the_sub_field('social_website_url'); ?>" title="<?php the_sub_field('social_website_name'); ?>" target="_blank"><?php echo $icon; ?></a></li>
+                                        <?php endwhile; ?>
+                                    </ul>
+                                <?php endif; ?>
+                            </div>
 
 						</div>
 						<div class="medium-60 columns0">
@@ -37,9 +37,10 @@
 					</div> <!-- end #inner-footer -->
 					<div class="row">
 						<div class="medium-12 columns">
-							<nav role="navigation">
-								<a href="<?php echo wp_login_url( admin_url() ); ?>" id="member-login">Member Login</a>
-								<?php joints_footer_links(); ?>
+							<nav role="navigation" class="footer__navigation">
+                                <?php joints_footer_links(); ?>
+                                <a href="<?php echo wp_login_url( admin_url() ); ?>" id="member-login">Member Login</a>
+
 								
 							</nav>
 						</div>
