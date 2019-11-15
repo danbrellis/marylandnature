f9017b0a875d85f271764432ee37c0acd79348a3
danbrellis <danbrellis@gmail.com>
2019-11-15 15:00:26 -0500

    Changes Collected by to Curated By for collections

--- wp-content/themes/marylandnature/parts/content-archive-nhsm_collections.php
+++ wp-content/themes/marylandnature/parts/content-archive-nhsm_collections.php
@@ -1,7 +1,7 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class('archive'); ?> role="article" itemscope itemtype="http://schema.org/WebPage">
     <h3 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h3>
     <p class="collection-meta">
-        <span class="collector">Collected by <strong><?php echo nhsm_get_formatted_collector($post); ?></strong></span>
+        <span class="collector">Curated by <strong><?php echo nhsm_get_formatted_collector($post); ?></strong></span>
         <?php /* &nbsp;<span class="middot">&middot;</span>&nbsp;
         <span class="specimen-no">Includes <strong>230 specimens</strong></span>
         &nbsp;<span class="middot">&middot;</span>&nbsp;
