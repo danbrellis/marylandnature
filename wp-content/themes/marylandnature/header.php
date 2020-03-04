30e249cb993d9bf2329eb841d585cc6483bc33b7
danbrellis <danbrellis@gmail.com>
2020-03-04 11:45:39 -0500

    Adds html site meta fields for SEO, FB, and Twitter

--- wp-content/themes/marylandnature/header.php
+++ wp-content/themes/marylandnature/header.php
@@ -40,8 +40,40 @@
 
 		<?php wp_head(); ?>
 
-		<!-- Drop Google Analytics here -->
-		<!-- end analytics -->
+        <?php
+        //Adds some extra meta for fb and twitter.
+        $post_id = get_queried_object_id();
+        $post_thumbnail_id = get_post_thumbnail_id($post_id);
+        $post_thumbnail_alt = get_post_meta($post_thumbnail_id, '_wp_attachment_image_alt', true);
+        $fb_post_thumbnail = wp_get_attachment_image_src($post_thumbnail_id, 'nhsm_headshot');
+        $share_excerpt = is_singular() ? get_the_excerpt() : get_the_archive_description();
+        if(get_post_type() === 'event'){
+            $dates = get_event_date_range($post_id);
+            if($dates){
+                $allday = get_post_meta($post_id, '_event_all_day', true);
+                $range = nhsm_format_date_range(strtotime($dates['start']), strtotime($dates['end']), boolval($allday));
+                $share_excerpt = sprintf('Join us %s. %s', $range, $share_excerpt);
+            }
+        }
+        ?>
+
+        <meta name="description" content="<?php echo esc_attr($share_excerpt); ?>" />
+
+        <meta property="og:url"                content="<?php echo home_url() . $_SERVER['REQUEST_URI']; ?>" />
+        <meta property="og:type"               content="<?php echo is_front_page() ? 'website' : 'article'; ?>" />
+        <meta property="og:title"              content="<?php echo wp_get_document_title(); ?>" />
+        <meta property="og:description"        content="<?php echo esc_attr($share_excerpt); ?>" />
+        <meta property="og:image"              content="<?php echo esc_attr($fb_post_thumbnail[0]); ?>" />
+        <meta property="og:image:width"        content="<?php echo esc_attr($fb_post_thumbnail[1]); ?>" />
+        <meta property="og:image:height"       content="<?php echo esc_attr($fb_post_thumbnail[2]); ?>" />
+        <meta property="og:image:alt"          content="<?php echo esc_attr($post_thumbnail_alt); ?>" />
+        <meta property="fb:app_id"             content="2882465385099613" />
+
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:site" content="@NHSM_Naturalist">
+        <meta name="twitter:title" content="<?php echo wp_get_document_title(); ?>">
+        <meta name="twitter:description" content="<?php echo esc_attr($share_excerpt); ?>">
+        <meta name="twitter:image" content="<?php echo esc_attr(get_the_post_thumbnail_url(null, 'nhsm_hbanner')); ?>">
 	</head>
 	
 	<!-- Uncomment this line if using the Off-Canvas Menu --> 
