6686414d3fafb8141c78ebf5913c06e455eb9cc8
danbrellis <danbrellis@gmail.com>
2020-05-05 12:31:42 -0400

    Adds excerpts for pages

--- wp-content/themes/marylandnature2/includes/custom-post-type.php
+++ wp-content/themes/marylandnature2/includes/custom-post-type.php
@@ -1,16 +1,6 @@
 <?php
-/* joints Custom Post Type Example
-This page walks you through creating 
-a custom post type and taxonomies. You
-can edit this one or copy the following code 
-to create another one. 
 
-I put this in a separate file so as to 
-keep it organized. I find it easier to edit
-and change things if they are concentrated
-in their own file.
-
-*/
+add_post_type_support( 'page', 'excerpt' );
 
 // adding the function to the Wordpress init
 add_action( 'init', 'nhsm_post_types');
