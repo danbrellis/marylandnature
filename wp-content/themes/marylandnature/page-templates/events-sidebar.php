51939fff5659d5fc45c407a410a0f03e1c2b674d
danbrellis <danbrellis@gmail.com>
2022-03-11 13:26:20 -0500

    Fixes bug on events-sidebar.php when the category ID needs to be sent to em_display_events, not slug.

--- wp-content/themes/marylandnature/page-templates/events-sidebar.php
+++ wp-content/themes/marylandnature/page-templates/events-sidebar.php
@@ -1,13 +1,15 @@
 <?php /* Template Name: Right Events Sidebar */ ?>
 <?php get_header();
+//get event-category id by slug:
+$event_category = get_term_by('name', get_post_field( 'post_name', get_post() ), 'event-category');
 $events_args = [
-    'categories' => [get_post_field( 'post_name', get_post() )],
     'show_event_thumbnail' => false,
     'show_event_excerpt' => false,
     'show_featured' => false,
     'show_past_events' => false,
     'no_events_message' => 'Apologies, but no upcoming ' . get_the_title() . ' events are scheduled.'
 ];
+if($event_category) $events_args['categories'] = [$event_category->term_id];
 $sidebar = '<h2 class="stringbean">Events</h2>' . em_display_events( $events_args ); ?>
     <div class="container main-container">
         <main class="main main--twoColumns" id="main">
